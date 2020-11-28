cc5d182f958edd7914d5e279557588855771055e
Federico <vgfede@gmail.com>
2020-11-28 16:44:50 +0100

    Add files via upload

--- lez09-19.11/ass9-ping/src/Server.java
+++ lez09-19.11/ass9-ping/src/Server.java
@@ -3,21 +3,37 @@ import java.net.*;
 import java.util.Random;
 
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SocketException {
 
-        DatagramSocket serverSocket;
-        try {
-            serverSocket = new DatagramSocket(6789);
-        } catch (SocketException e) {
-            e.printStackTrace();
-            return;
+        //Controllo degli argomenti
+        int port;
+        int seed = 0;
+        if (args.length==0) {
+            port = 6789;    //Porta di default se non viene specificata da terminale
+        } else {
+            try {
+                port = Integer.parseInt(args[0]);
+                if (args.length==2) seed = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("ERR -port -seed : not numbers");
+                System.out.println("Usage: java Server <port> <seed>");
+                return;
+            }
         }
 
+        //Apro il server in ascolto
+        DatagramSocket serverSocket = new DatagramSocket(port);
         byte[] receive = new byte[20];
         DatagramPacket receivePacket = new DatagramPacket(receive, receive.length);
-        Random rand = new Random();
 
-        System.out.println("Server attivo sulla porta 6789\n");
+        //Random per le probabilita' di delay e perdita
+        Random rand;
+        if (seed==0) rand = new Random();
+        else rand = new Random(seed);
+
+        System.out.printf("Server attivo sulla porta %d ", port);
+        if (seed==0) System.out.println("senza seed\n");
+        else System.out.printf("con seed=%d\n\n", seed);
 
         while (true) {
             try {
